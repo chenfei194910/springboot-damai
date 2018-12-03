b09f3c569115e35659d60f15f7fb835ef0bdf5fb
ChenFei <ChenFei@DESKTOP-VNFM9Q4>
2018-12-03 18:02:32 +0800

    redis

--- src/test/java/com/springboot/three/RedisAutoTest.java
+++ src/test/java/com/springboot/three/RedisAutoTest.java
@@ -1,5 +1,6 @@
 package com.springboot.three;
 
+import org.apache.logging.log4j.util.Strings;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,8 +17,14 @@ public class RedisAutoTest {
 
     @Test
     public void save(){
-        stringRedisTemplate.opsForValue().set("zzp","big z");
-        Assert.assertEquals("big z",stringRedisTemplate.opsForValue().get("zzp"));
+    	if(Strings.isNotBlank(stringRedisTemplate.opsForValue().get("zzp"))) {
+    		stringRedisTemplate.delete("zzp");
+    		stringRedisTemplate.opsForValue().set("zzp","test1");    		
+    	}
+//        Assert.assertEquals("big z",stringRedisTemplate.opsForValue().get("zzp"));
+        
+        stringRedisTemplate.opsForValue().set("user1", "felix");
+        Assert.assertEquals("felix", stringRedisTemplate.opsForValue().get("user1"));
     }
 
 }
